776c2d70c6448c7d156e893018e0f03b61233485
DESKTOP-466OFPF\AdminPC <zhukovartemvl@gmail.com>
2020-11-28 19:13:06 +0200

    Navigation, View binding and design fixes

--- AndroidAcademy/app/src/main/java/com/zhukovartemvl/androidacademy/screens/MovieDetailsFragment.kt
+++ AndroidAcademy/app/src/main/java/com/zhukovartemvl/androidacademy/screens/MovieDetailsFragment.kt
@@ -5,16 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.zhukovartemvl.androidacademy.R
+import com.zhukovartemvl.androidacademy.databinding.FragmentMovieDetailsBinding
+import com.zhukovartemvl.androidacademy.databinding.FragmentMoviesListBinding
 
 
 class MovieDetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentMovieDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+        inflater.inflate(R.layout.fragment_movie_details, container, false)
+        binding = FragmentMovieDetailsBinding.inflate(inflater)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
 
 }
--- AndroidAcademy/app/src/main/java/com/zhukovartemvl/androidacademy/screens/MoviesListFragment.kt
+++ AndroidAcademy/app/src/main/java/com/zhukovartemvl/androidacademy/screens/MoviesListFragment.kt
@@ -5,16 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.zhukovartemvl.androidacademy.R
+import com.zhukovartemvl.androidacademy.databinding.FragmentMoviesListBinding
 
 
 class MoviesListFragment : Fragment() {
+
+    private lateinit var binding: FragmentMoviesListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_movies_list, container, false)
+        inflater.inflate(R.layout.fragment_movies_list, container, false)
+        binding = FragmentMoviesListBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.sampleMovieItem.movieItemBase.setOnClickListener {
+            findNavController().navigate(R.id.toMovieDetails)
+        }
     }
 
 }
